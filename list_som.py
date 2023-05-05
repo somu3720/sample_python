8b72b65112a3624dfa7b765a7ec5f22036fc0979
Somnath Pandey <somu3720@gmail.com>
2023-05-05 11:38:06 +0000

    Create list_som.py

---
+++ list_som.py
@@ -0,0 +1,16 @@
+import os
+
+List1 = [8, 9, 3, 6, 1]
+List1.reverse()
+print("the reversed list is", List1)
+
+List2 = [91, 56, 78, 45, 72, 78, 87, 94, 65, 346]
+List2.sort()
+print("the sorted list is", List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3 =", List3)
+
+indexvalue = List2[2:6]
+print(the index value are", indexvalue)
